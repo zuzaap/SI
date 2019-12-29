af28aa8c52a398fb8d20a65ec1ca3ea108b1952a
zuzaap <44367673+zuzaap@users.noreply.github.com>
2019-12-29 18:02:46 +0100

    Update GUI.java

--- src/main/java/com/sample/GUI.java
+++ src/main/java/com/sample/GUI.java
@@ -4,43 +4,38 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class GUI extends JFrame{
-	public static JFrame jframe;
-	
-	public static void showStart() {
-		JOptionPane.showMessageDialog(jframe, "Electronic Music for people who don't like electronic music","Start",JOptionPane.PLAIN_MESSAGE);
-	}
-	public static String questionWindow(String[] possible, String question) {
-		String answer;
-		String[] answers = possible;
-		String[] koniec = {"koniec"};
-		int selected;
-		//String[] answer, String question
-		if(answers.length == 0) {
-			selected = JOptionPane.showOptionDialog(null, question, "Question", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, koniec, null);
-			
-		}else {
-			selected = JOptionPane.showOptionDialog(null,
-					question,
-					"Question",
-					JOptionPane.DEFAULT_OPTION, 
-					JOptionPane.QUESTION_MESSAGE,
-					null, 
-					answers, 
-					null);
-		}
-		
-		answer = answers[selected];
-		
-		return answer;
-	}
-	
-	public static void showAlbums(String album) {
-		JOptionPane.showMessageDialog(null,
-				"You should listen to:" + album);
-	}
-	
-	public static void showInit(String album) {
-		JOptionPane.showMessageDialog(null,album);
-	}
+    public static JFrame jframe;
+
+    public static void showStart() {
+        JOptionPane.showMessageDialog(jframe, "Electronic Music for people who don't like electronic music","Start",JOptionPane.PLAIN_MESSAGE);
+    }
+    public static String questionWindow(String[] possible, String question) {
+        String answer;
+        String[] answers = possible;
+        String[] koniec = {"koniec"};
+        int selected;
+        if(answers == null) {
+            selected = JOptionPane.showOptionDialog(null, question, "Question", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, koniec, null);
+            return "Koniec!";
+        }else {
+            selected = JOptionPane.showOptionDialog(null,
+                    question,
+                    "Question",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    answers,
+                    null);
+        }
+
+        answer = answers[selected];
+
+        return answer;
+    }
+
+    public static void showInit(String album) {
+        JOptionPane.showMessageDialog(null,album);
+    }
 
 }
+
